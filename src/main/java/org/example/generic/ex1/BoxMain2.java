d187ac1e0456c0e89681d1cbfad317478da2e082
gdrffg <gdrffg@gmail.com>
2025-01-30 15:54:49 +0900

    generic-1

---
+++ src/main/java/org/example/generic/ex1/BoxMain2.java
@@ -0,0 +1,20 @@
+package org.example.generic.ex1;
+
+public class BoxMain2 {
+    public static void main(String[] args) {
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.set(10);
+        Integer integer = (Integer) integerBox.get(); // 자식은 부모를 담을 수 있다.
+        System.out.println("integer = " + integer);
+
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.set("hello");
+        String string = (String) stringBox.get();
+        System.out.println("string = " + string);
+
+        integerBox.set("afafwe");
+        Integer result = (Integer) integerBox.get(); // String -> Integer 캐스팅
+        System.out.println("result = " + result);
+
+    }
+}
---
+++ src/main/java/org/example/generic/ex1/IntegerBox.java
@@ -0,0 +1,13 @@
+package org.example.generic.ex1;
+
+public class IntegerBox {
+    private Integer value;
+
+    public void set(Integer value){
+        this.value = value;
+    }
+
+    public Integer get(){
+        return this.value;
+    }
+}
