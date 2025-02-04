f7bf1c190fdce838d830364d1aaa8cea151cf1db
gdrffg <gdrffg@gmail.com>
2025-02-04 15:18:48 +0900

    volatile

---
+++ src/main/java/org/example/thread/volatile1/VolatileCountMain2.java
@@ -0,0 +1,38 @@
+package org.example.thread.volatile1;
+
+import static org.example.thread.util.MyLogger.log;
+import static org.example.thread.util.ThreadUtils.sleep;
+
+public class VolatileCountMain2 {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        t.start();
+        sleep(1000);
+        task.flag = false;
+        log("flag = " + task.flag + ", count = " + task.count + " in main");
+
+    }
+
+    static class MyTask implements Runnable {
+        //boolean flag = true;
+        //long count;
+
+        volatile boolean flag = true;
+        volatile long count;
+
+
+        @Override
+        public void run() {
+            while (flag) {
+                count++;
+                //1억번에 한번씩 출력
+                if (count % 100_000_000 == 0) {
+                    //주석 처리 한다면...
+                    log("flag = " + flag + ", count = " + count + " in while()");
+                }
+            }
+            log("flag = " + flag + ", count = " + count + " 종료");
+        }
+    }
+}
